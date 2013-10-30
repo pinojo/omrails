2e0fa3a33f7c50fb678d228e9b36e1d92e8a57ab
pinojo <pinojo@gmail.com>
2013-10-29 21:58:57 -0500

    Try Fog Paperclip RS Cloud Files again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,18 @@ Omrails::Application.configure do
   #In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
+ Paperclip::Attachment.default_options.update({
+     :path => "images/:class/:id/:attachment/:style/img_:fingerprint",
+     :storage => :fog,
+     :fog_credentials => {
+       :provider           => 'Rackspace',
+       :rackspace_username => ENV['RACKSPACE_USERNAME'],
+       :rackspace_api_key  => ENV['RACKSPACE_API_KEY'],
+       :persistent => false
+     },
+     :fog_directory => 'omrails',
+     :fog_public => true,
+     :fog_host => 'http://b4a40795c3617b91d320-25e3ca868ca0d6d10545d68f23f5a8ed.r9.cf2.rackcdn.com'
+   })
+     
 end
